0c452c0cb2127384e2406b5a38d2452afca0a1c3
GitHub Action <action@github.com>
2021-11-02 01:25:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xiongjl/intellijplatformpluginsdktomodel/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xiongjl.intellijplatformpluginsdktomodel.services
+
+import com.github.xiongjl.intellijplatformpluginsdktomodel.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
